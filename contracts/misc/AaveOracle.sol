930a0d2252c3ff35c129647ea74e7a3d31bc0807
anirudh <anirudhtx@gmail.com>
2023-04-21 17:54:04 +0000

    add pyth as dependencies

--- contracts/misc/AaveOracle.sol
+++ contracts/misc/AaveOracle.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.10;
 
-import '@pythnetwork/pyth-sdk-solidity/IPyth.sol';
-import '@pythnetwork/pyth-sdk-solidity/PythStructs.sol';
+import '../dependencies/pyth/IPyth.sol';
+import '../dependencies/pyth/PythStructs.sol';
 import {Errors} from '../protocol/libraries/helpers/Errors.sol';
 import {IACLManager} from '../interfaces/IACLManager.sol';
 import {IPoolAddressesProvider} from '../interfaces/IPoolAddressesProvider.sol';
