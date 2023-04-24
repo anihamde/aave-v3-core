cd9fee339c59fbff4fe233240863ee5757182bdd
Anirudh Suresh <ani@anirudhs-mbp.lan>
2023-04-24 13:25:17 -0500

    add changes to oracle specific tests

--- test-suites/aave-oracle.spec.ts
+++ test-suites/aave-oracle.spec.ts
@@ -11,6 +11,7 @@ import {
   MintableERC20,
   MockAggregator,
 } from '@aave/deploy-v3';
+import { aave } from '@aave/deploy-v3/dist/types/typechain/factories';
 
 makeSuite('AaveOracle', (testEnv: TestEnv) => {
   let snap: string;
@@ -53,30 +54,46 @@ makeSuite('AaveOracle', (testEnv: TestEnv) => {
       .to.emit(aaveOracle, 'AssetSourceUpdated')
       .withArgs(mockToken.address, mockAggregator.address);
 
-    const sourcesPrices = await (
-      await aaveOracle.getAssetsPrices([mockToken.address])
-    ).map((x) => x.toString());
+    // update mock Pyth with specified price
+    await aaveOracle.updateWithPriceFeedUpdateData(
+      mockAggregator.address,
+      assetPrice,
+      1,
+      0,
+      assetPrice,
+      1,
+      1_600_000_000_000
+    );
+
+    const sourcesPrices = (await aaveOracle.getAssetsPrices([mockToken.address])).map((x) =>
+      x.toString()
+    );
+
     expect(await aaveOracle.getSourceOfAsset(mockToken.address)).to.be.eq(mockAggregator.address);
     expect(await aaveOracle.getAssetPrice(mockToken.address)).to.be.eq(assetPrice);
     expect(sourcesPrices).to.eql([assetPrice]);
   });
 
   it('Owner update an existing asset source', async () => {
-    const { poolAdmin, aaveOracle, dai } = testEnv;
+    const { poolAdmin, aaveOracle, dai, aave } = testEnv;
 
     // DAI token has already a source
     const daiSource = await aaveOracle.getSourceOfAsset(dai.address);
     expect(daiSource).to.be.not.eq(ZERO_ADDRESS);
 
-    // Update DAI source
-    await expect(
-      aaveOracle.connect(poolAdmin.signer).setAssetSources([dai.address], [mockAggregator.address])
-    )
-      .to.emit(aaveOracle, 'AssetSourceUpdated')
-      .withArgs(dai.address, mockAggregator.address);
+    const daiPrice = MOCK_CHAINLINK_AGGREGATORS_PRICES.DAI;
+    expect(await aaveOracle.getAssetPrice(dai.address)).to.be.eq(daiPrice);
 
-    expect(await aaveOracle.getSourceOfAsset(dai.address)).to.be.eq(mockAggregator.address);
-    expect(await aaveOracle.getAssetPrice(dai.address)).to.be.eq(assetPrice);
+    // Update DAI source to AAVE source
+    const aaveSource = await aaveOracle.getSourceOfAsset(aave.address);
+    await expect(aaveOracle.connect(poolAdmin.signer).setAssetSources([dai.address], [aaveSource]))
+      .to.emit(aaveOracle, 'AssetSourceUpdated')
+      .withArgs(dai.address, aaveSource);
+
+    const aavePrice = MOCK_CHAINLINK_AGGREGATORS_PRICES.AAVE;
+
+    expect(await aaveOracle.getSourceOfAsset(dai.address)).to.be.eq(aaveSource);
+    expect(await aaveOracle.getAssetPrice(dai.address)).to.be.eq(aavePrice);
   });
 
   it('Owner tries to set a new asset source with wrong input params (revert expected)', async () => {
@@ -110,6 +127,17 @@ makeSuite('AaveOracle', (testEnv: TestEnv) => {
   it('Get price of BASE_CURRENCY asset with registered asset source for its address', async () => {
     const { poolAdmin, aaveOracle, weth } = testEnv;
 
+    // update mock Pyth with specified price
+    await aaveOracle.updateWithPriceFeedUpdateData(
+      mockAggregator.address,
+      12,
+      1,
+      0,
+      12,
+      1,
+      1_600_000_000_000
+    );
+
     // Add asset source for BASE_CURRENCY address
     await expect(
       aaveOracle.connect(poolAdmin.signer).setAssetSources([weth.address], [mockAggregator.address])
@@ -118,9 +146,7 @@ makeSuite('AaveOracle', (testEnv: TestEnv) => {
       .withArgs(weth.address, mockAggregator.address);
 
     // Check returns the fixed price BASE_CURRENCY_UNIT
-    expect(await aaveOracle.getAssetPrice(weth.address)).to.be.eq(
-      MOCK_CHAINLINK_AGGREGATORS_PRICES.WETH
-    );
+    expect(await aaveOracle.getAssetPrice(weth.address)).to.be.eq(12);
   });
 
   it('Get price of asset with no asset source', async () => {
@@ -140,6 +166,17 @@ makeSuite('AaveOracle', (testEnv: TestEnv) => {
   it('Get price of asset with 0 price and no fallback price', async () => {
     const { poolAdmin, aaveOracle } = testEnv;
     const zeroPriceMockAgg = await deployMockAggregator('0');
+
+    // update mock Pyth with specified price
+    await aaveOracle.updateWithPriceFeedUpdateData(
+      zeroPriceMockAgg.address,
+      0,
+      1,
+      0,
+      0,
+      1,
+      1_600_000_000_000
+    );
 
     // Asset has no source
     expect(await aaveOracle.getSourceOfAsset(mockToken.address)).to.be.eq(ZERO_ADDRESS);
@@ -161,6 +198,17 @@ makeSuite('AaveOracle', (testEnv: TestEnv) => {
     const { poolAdmin, aaveOracle, oracle } = testEnv;
     const zeroPriceMockAgg = await deployMockAggregator('0');
     const fallbackPrice = oneEther;
+
+    // update mock Pyth with specified price
+    await aaveOracle.updateWithPriceFeedUpdateData(
+      zeroPriceMockAgg.address,
+      0,
+      1,
+      0,
+      0,
+      1,
+      1_600_000_000_000
+    );
 
     // Register price on FallbackOracle
     expect(await oracle.setAssetPrice(mockToken.address, fallbackPrice));
